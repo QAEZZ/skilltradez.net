719c5d3e580bf403c2ecb1049e8933b4004e9e54
QAEZZ <russian.devv@gmail.com>
2023-10-25 10:44:54 -0500

    added intro animation

---
+++ js/matrix.js
@@ -0,0 +1,82 @@
+const canvas = document.getElementById('matrix-effect');
+const context = canvas.getContext('2d');
+
+const sleep = (ms) => new Promise((resolve) => {
+    const start = performance.now();
+    const animate = (timestamp) => {
+        if (timestamp - start < ms) {
+            requestAnimationFrame(animate);
+        } else {
+            resolve();
+        }
+    };
+    animate(start);
+});
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const alphabet = "SKILLTRADEZ   ";
+
+const fontSize = 12;
+const columns = canvas.width / fontSize;
+
+const rainDrops = [];
+
+for (let x = 0; x < columns; x++) {
+    rainDrops[x] = 1;
+}
+
+const duration = 8500;
+let startTime;
+let animationFrameId;
+let alphabetIndex = 0; // To keep track of the position in alphabet
+
+const animate = async () => {
+    let currentTime;
+    let elapsedTime;
+
+    while (!startTime || elapsedTime < duration) {
+        currentTime = performance.now();
+        elapsedTime = currentTime - (startTime || currentTime);
+
+        if (elapsedTime >= duration - 3100) {
+            canvas.classList.add('fade-out');
+        }
+
+        context.fillStyle = 'rgba(0, 0, 0, 0.05)';
+        context.fillRect(0, 0, canvas.width, canvas.height);
+
+        context.fillStyle = '#0F0';
+        context.font = fontSize + 'px monospace';
+
+        for (let i = 0; i < rainDrops.length; i++) {
+            const text = alphabet.charAt(alphabetIndex % alphabet.length);
+
+            context.fillText(text, i * fontSize, rainDrops[i] * fontSize);
+
+            if (rainDrops[i] * fontSize > canvas.height && Math.random() > 0.975) {
+                rainDrops[i] = 0;
+            }
+            rainDrops[i]++;
+            alphabetIndex++; // Increment the alphabet index
+        }
+
+        if (!startTime) {
+            startTime = currentTime;
+        }
+
+        if (elapsedTime < duration) {
+            animationFrameId = requestAnimationFrame(animate);
+        } else {
+            await sleep(5500);
+            cancelAnimationFrame(animationFrameId);
+        }
+
+        await sleep(25); // A small delay to control the frame rate
+    }
+};
+
+// Start the animation
+animate();
+
